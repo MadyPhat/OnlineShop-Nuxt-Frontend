fed9a1dc9371eaf7d61a0067d6c84afd19ba70f0
Mady Phat <phatmady21@gmail.com>
2021-09-28 09:09:34 +0700

    main

---
+++ admin/layouts/default.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="page-wrapper">
+
+    <mobile-header></mobile-header>
+
+    <admin-sidebar></admin-sidebar>
+
+    <div class="page-container">
+
+      <admin-header></admin-header>
+
+      <Nuxt />
+
+    </div>
+
+  </div>
+</template>
+
+<script>
+    import AdminHeader from "../components/partails/AdminHeader";
+    import AdminSidebar from "../components/partails/AdminSidebar";
+    import MobileHeader from "../components/partails/MobileHeader";
+
+    export default {
+        components: {AdminHeader, AdminSidebar, MobileHeader},
+mounted() {
+          const script = document.createElement("script")
+          script.src = "/js/main.js"
+          script.type = "text/javascript"
+          document.head.appendChild(script);
+
+          this.checkUserLogin();
+        },
+methods: {
+          checkUserLogin() {
+            // send request to check if the user is logged otherwise redirect him to login screen
+                if(localStorage.getItem('auth_token') !== null && localStorage.getItem('auth_token') !== undefined) {
+                  this.$axios.setHeader('Authorization', "Bearer " + localStorage.getItem('auth_token'));
+
+                  this.$axios.$get('api/check-login').then(response => {
+                    if (response.success !== 1) {
+
+                      localStorage.removeItem('auth_token');
+                      localStorage.removeItem('is_authenticated');
+                      localStorage.removeItem('user_data');
+
+                      this.$router.push('/login');
+                    }
+                  }).catch(err => {
+                    localStorage.removeItem('auth_token');
+                    localStorage.removeItem('is_authenticated');
+                    localStorage.removeItem('user_data');
+
+                    this.$router.push('/login');
+                  });
+                }
+          }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ nuxt.config.js
@@ -0,0 +1,87 @@
+export default {
+  /*
+  ** Nuxt rendering mode
+  ** See https://nuxtjs.org/api/configuration-mode
+  */
+  mode: 'universal',
+  /*
+  ** Nuxt target
+  ** See https://nuxtjs.org/api/configuration-target
+  */
+  target: 'server',
+  /*
+  ** Headers of the page
+  ** See https://nuxtjs.org/api/configuration-head
+  */
+  head: {
+    title: 'Online Shop',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Online Shop' }
+    ],
+    link: [
+      { rel: 'stylesheet', type: 'text/css', href: '/css/bootstrap.min.css' },
+      { rel: 'stylesheet', type: 'text/css', href: '/css/font-awesome.min.css' },
+      { rel: 'stylesheet', type: 'text/css', href: '/css/prettyPhoto.css' },
+      { rel: 'stylesheet', type: 'text/css', href: '/css/main.css' },
+      { rel: 'stylesheet', type: 'text/css', href: '/css/responsive.css' },
+    ],
+    script: [
+      { src: '/js/jquery.js', type: 'text/javascript' },
+      { src: '/js/bootstrap.min.js', type: 'text/javascript' },
+      { src: '/js/jquery.scrollUp.min.js', type: 'text/javascript' },
+      { src: '/js/jquery.prettyPhoto.js', type: 'text/javascript' },
+      { src: '/js/main.js', type: 'text/javascript' },
+    ]
+  },
+  /*
+  ** Global CSS
+  */
+  css: [
+  ],
+  /*
+  ** Plugins to load before mounting the App
+  ** https://nuxtjs.org/guide/plugins
+  */
+  plugins: [
+  ],
+  /*
+  ** Auto import components
+  ** See https://nuxtjs.org/api/configuration-components
+  */
+  components: true,
+  /*
+  ** Nuxt.js dev-modules
+  */
+  buildModules: [
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    // Doc: https://bootstrap-vue.js.org
+    '@nuxtjs/axios',
+  ],
+  axios: {
+    baseURL: 'http://localhost:8000/'
+  },
+  /*
+  ** Build configuration
+  ** See https://nuxtjs.org/api/configuration-build/
+  */
+  build: {
+    devMiddleware: {
+      headers: {
+        'Cache-Control': 'no-store',
+        Vary: '*'
+      }
+    }
+  },
+  srcDir: __dirname,
+  buildDir: '.nuxt/frontend',
+
+  env: {
+    PAYPAL_CLIENT_ID: "AUuH8WslXfXRrv9bh-OwtJuoJy3OC_gcwXu0YQ24bIVbcz464e5h1V9KG7yQevAe4kn7Eo4fx3FBbZXE"   // add your client ID that you created in the previous step
+  }
+}
